dae6306883cb064e70bde0604a3ed3fba749448d
Enrique <enrique.mora@es.nestle.com>
2026-01-17 17:58:03 +0100

    json to csv

--- dqn-atari/04_rllib_json_to_csv.py
+++ dqn-atari/04_rllib_json_to_csv.py
@@ -3,17 +3,22 @@ import debugpy
 import pandas as pd
 import torch
 from ray.rllib.offline.json_reader import JsonReader
-from oppe_utils import load_json_to_df
+from oppe_utils import load_json_to_df_max
 
 
 def json_to_csv(rllib_json, csv_path):
     beh_reader = JsonReader(rllib_json)
-    beh_df = load_json_to_df(beh_reader, 10000)
+    beh_df, eps, steps = load_json_to_df_max(beh_reader, 10000)
+    print(f'loaded JSON: {rllib_json}')
+    print(f"Transformed {eps} episodes with a total of {steps} steps")
     beh_df.to_csv(csv_path)
 
 
 if __name__ == '__main__':
 
-    BEH_EPISODES_JSON_TEST = '/opt/ml/code/episodes/120820251600/011125_01_generated_rllib_ppo_rllib_seed_0000_10000eps_300steps_exp_0'
-    CSV_PATH =  '/opt/ml/code/episodes/120820251600/011125_01_generated_rllib_ppo_rllib_seed_0000_10000eps_300steps_exp_0.csv'
-    json_to_csv(BEH_EPISODES_JSON_TEST, CSV_PATH)
+    BEH_EPISODES_JSON_VAL = '/opt/ml/code/episodes/120820251600/011125_generated_rllib_ppo_rllib_seed_0000_1000eps_300steps_exp_0'
+    EVAL_EPISODES_JSON = '/opt/ml/code/episodes/130820251600/011125_generated_rllib_ppo_rllib_seed_0000_1000eps_300steps_exp_0'
+    BEH_EPISODES_JSON_TRAIN = '/opt/ml/code/episodes/120820251600/011125_01_generated_rllib_ppo_rllib_seed_0000_10000eps_300steps_exp_0'
+    JSON_TO_CONVERT =  BEH_EPISODES_JSON_VAL
+    CSV_PATH =  '/opt/ml/code/episodes/120820251600/011125_generated_rllib_ppo_rllib_seed_0000_1000eps_300steps_exp_0.csv'
+    json_to_csv(JSON_TO_CONVERT, CSV_PATH)
